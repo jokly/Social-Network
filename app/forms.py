dfeb03a5c088f1cda17fa464ac9472f29e5408b8
Slasten Tikhon <slasten32@gmail.com>
2018-04-28 13:51:47 +1000

    Added register and login

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SelectField, IntegerField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     login = StringField('Login', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
-    
+
+class RegistrationForm(FlaskForm):
+    login = StringField('Login', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(),
+        EqualTo('password')])
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    sex = SelectField('Sex', validators=[DataRequired()], choices=[('male', 'Male'), ('female', 'Female')])
+    city = StringField('City', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_login(self, login):
+        user = User.query.filter_by(login=login.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different login')
+
+    def validate_age(self, age):
+        age = int(age.data)
+
+        if age <= 0 or age > 100:
+            raise ValidationError('Invalid age')
--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120))
     password_hash = db.Column(db.String(128))
     name = db.Column(db.String(64))
     surname = db.Column(db.String(64))
@@ -20,3 +21,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.login) 
+
+@login.user_loader
+def loader_user(id):
+    return User.query.get(int(id))
