25b7e92e05ac05007eb201f431d1dedb913c959e
Slasten Tikhon <slasten32@gmail.com>
2018-05-25 15:37:50 +1000

    Added age validator to edit profile

--- app/forms.py
+++ app/forms.py
@@ -32,6 +32,6 @@ class EditProfileForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     name = StringField('Name', validators=[DataRequired()])
     surname = StringField('Surname', validators=[DataRequired()])
-    age = IntegerField('Age', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(1, 100, 'Invalid age')])
     city = StringField('City', validators=[DataRequired()])
     submit = SubmitField('Submit')
