09d3d0f42e16f99fed17a4012d8a51624aa1bca0
Slasten Tikhon <slasten32@gmail.com>
2018-06-02 00:00:38 +1000

    First-depth search in get comments query

--- sql/get_comments.sql
+++ sql/get_comments.sql
@@ -6,17 +6,19 @@ DECLARE
 BEGIN
 	OPEN _result FOR 
     WITH RECURSIVE r AS (
-        SELECT post.id, post.parent_post, post.text, 0 as level
+        SELECT post.id, post.parent_post, post.text, 0 as level,
+        post.id || '' as path
         FROM post
         WHERE post.id = curr_post_id
         
         UNION
         
-        SELECT post.id, post.parent_post, post.text, r.level + 1 as level
+        SELECT post.id, post.parent_post, post.text, r.level + 1 as level,
+        r.path || post.id as path
         FROM post
             JOIN r
             ON post.parent_post = r.id
-    ) SELECT * FROM r;
+    ) SELECT * FROM r ORDER BY path;
     RETURN _result;
 END
 $$ LANGUAGE plpgsql;
